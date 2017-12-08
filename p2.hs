f3c80e05a2c9adb6f10c842e853e868a29db739f
natchensing <nataliechen1024@hotmail.com>
2017-12-08 15:18:34 -0800

    updated formatting

--- p2.hs
+++ p2.hs
@@ -1,3 +1,4 @@
+
 putTodo :: (Int, String) -> IO ()
 putTodo (n, todo) = putStrLn (show n ++ ": " ++ todo)
 
@@ -7,18 +8,19 @@ prompt todos = do
     putStrLn "Current TODO list:"
     mapM_ putTodo (zip [0..] todos)
     command <- getLine
-    interpret command todos
+    functions command todos
 
-interpret :: String -> [String] -> IO ()
-interpret ('+':' ':todo) todos = prompt (todo:todos)
-interpret ('-':' ':num ) todos =
+
+functions :: String -> [String] -> IO ()
+functions ('+':' ':todo) todos = prompt (todo:todos)
+functions ('-':' ':num ) todos =
     case delete (read num) todos of
         Nothing -> do
-            putStrLn "No TODO entry matches the given number"
+            putStrLn "No TODO entry matches that number!"
             prompt todos
         Just todos' -> prompt todos'
-interpret  "q"           todos = return ()
-interpret  command       todos = do
+functions "q"           todos = return ()
+functions  command       todos = do
     putStrLn ("Invalid command: `" ++ command ++ "`")
     prompt todos
 
@@ -27,9 +29,7 @@ delete 0 (_:as) = Just as
 delete n (a:as) = do
     as' <- delete (n - 1) as
     return (a:as')
-delete _  []    = Nothing
-
--- calendar part
+delete _ [] = Nothing
 
 data DayOfWeek
     = Sunday | Monday | Tuesday | Wednesday | Thursday | Friday | Saturday
@@ -50,7 +50,7 @@ pad day = case show day of
     cs  -> cs
 
 month :: Month -> DayOfWeek -> Int -> String
-month m startDay maxDay = show m ++ " 2015\n" ++ week ++ spaces Sunday
+month m startDay maxDay = show m ++ " 2017\n" ++ week ++ spaces Sunday
   where
     week = "Su Mo Tu We Th Fr Sa\n"
 
@@ -59,26 +59,20 @@ month m startDay maxDay = show m ++ " 2015\n" ++ week ++ spaces Sunday
 
     days Sunday    n | n > maxDay = "\n"
     days _         n | n > maxDay = "\n\n"
-    days Saturday  n              = pad n ++ "\n" ++ days  Sunday    (succ n)
-    days day       n              = pad n ++ " "  ++ days (next day) (succ n)
+    days Saturday  n = pad n ++ "\n" ++ days  Sunday    (succ n)
+    days day       n = pad n ++ " "  ++ days (next day) (succ n)
 
-year = month January   Thursday  31
-    ++ month February  Sunday    28
-    ++ month March     Sunday    31
-    ++ month April     Wednesday 30
-    ++ month May       Friday    31
-    ++ month June      Monday    30
-    ++ month July      Wednesday 31
-    ++ month August    Saturday  31
-    ++ month September Tuesday   30
-    ++ month October   Thursday  31
-    ++ month November  Sunday    30
-    ++ month December  Tuesday   31
+year = month January   Sunday       31 ++ month February  Wednesday    28
+    ++ month March     Wednesday    31 ++ month April     Saturday     30
+    ++ month May       Monday       31 ++ month June      Thursday     30
+    ++ month July      Saturday     31 ++ month August    Tuesday      31
+    ++ month September Friday       30 ++ month October   Sunday       31
+    ++ month November  Wednesday    30 ++ month December  Friday       31
 
 main = do
-    putStrLn "Commands:"
-    putStrLn "+ <String> - Add a TODO entry"
-    putStrLn "- <Int>    - Delete the numbered entry"
-    putStrLn "q          - Quit"
     putStr year
+    putStrLn "Commands:"
+    putStrLn "+ <String>  Add a TODO entry"
+    putStrLn "- <Int>     Delete the numbered entry"
+    putStrLn "q           Quit"
     prompt []
